4162ab9b6eca28a9729ca75f31e728d0dbe75b2a
SandisKlotins <sandis.klotins@gmail.com>
2023-04-10 23:40:20 +0300

    added workaround in game bug, more gui

--- Game.py
+++ Game.py
@@ -13,13 +13,13 @@ class Game:
         self.player_one_goes_first = player_one_goes_first
         self.turns = turns
         self.turn: int = 0
-        self.previous_id: int = 0
-        self.human_health: int
-        self.human_shields: int
-        self.ai_health: int
-        self.ai_shields: int
+        self.id: int = 0
+        self.human_health: int = self.turns[0][0][self.human_player]['health']
+        self.human_shields: int = self.turns[0][0][self.human_player]['shields']
+        self.ai_health: int = self.turns[0][0][self.ai_player]['health']
+        self.ai_shields: int = self.turns[0][0][self.ai_player]['shields']
 
-    def processFirstTurn(self) -> None:
+    def processFirstTurn(self, spell: str) -> None:
         # Play out first turn
         # Set game to start on 1st turn (1st choice)
         self.turn = self.turn + 1
@@ -52,17 +52,16 @@ class Game:
             self.human_shields = best_option[self.human_player]['shields']
             self.ai_health = best_option[self.ai_player]['health']
             self.ai_shields = best_option[self.ai_player]['shields']
-            self.previous_id = best_option['id']
+            self.id = best_option['id']
 
         else:
             # Process human turn
-            spell: str = self.player.input()
-            option: dict = [node for node in self.turns[self.turn] if node['spell'] == spell][0]
+            option: dict = [node for node in options if node['spell'] == spell][0]
             self.human_health = option[self.human_player]['health']
             self.human_shields = option[self.human_player]['shields']
             self.ai_health = option[self.ai_player]['health']
             self.ai_shields = option[self.ai_player]['shields']
-            self.previous_id = option['id']
+            self.id = option['id']
 
         print('____________________________________')
         print(f"Player {options[0]['player']} attacks with {spell}")
@@ -71,50 +70,75 @@ class Game:
 
         self.turn = self.turn + 1
 
-    def processTurn(self) -> None:
+    def processTurn(self, spell: str) -> None:
         # Play out regular turn
-        options: list[dict] = [option for option in self.turns[self.turn] if self.previous_id in option['previous_id']]
 
-        # Process AI turn
-        if options[0]['player'] == self.ai_player:
-            # Set default as bad option
-            max_rating: int = -1
-            # If there are no good options use the first element in the list
-            best_option: dict = options[0]
+        if self.human_health > 0 and self.ai_health > 0:
 
-            for option in options:
-                if max_rating < option['rating']:
-                    max_rating = option['rating']
-                    best_option = option
+            options: list[dict] = [option for option in self.turns[self.turn] if self.id in option['previous_id']]
 
-                # Subroutine: in case ai values its best option and the current option the same.
-                # Check if options product is better. This helps AI make overall better choices if the human player is not playing optimally
-                elif max_rating == option['rating']:
-                    option_product: int = option[self.human_player]['health'] + option[self.human_player]['shields']
-                    best_option_product: int = best_option[self.human_player]['health'] + best_option[self.human_player]['shields']
+            # Process AI turn
+            if options[0]['player'] == self.ai_player:
+                # Set default as bad option
+                max_rating: int = -1
+                # If there are no good options use the first element in the list
+                best_option: dict = options[0]
 
-                    if option_product < best_option_product:
+                for option in options:
+                    if max_rating < option['rating']:
+                        max_rating = option['rating']
                         best_option = option
 
-            spell: str = best_option['spell']
-            self.human_health = best_option[self.human_player]['health']
-            self.human_shields = best_option[self.human_player]['shields']
-            self.ai_health = best_option[self.ai_player]['health']
-            self.ai_shields = best_option[self.ai_player]['shields']
-            self.previous_id = best_option['id']
+                    # Subroutine: in case ai values its best option and the current option the same.
+                    # Check if options product is better. This helps AI make overall better choices if the human player is not playing optimally
+                    elif max_rating == option['rating']:
+                        option_product: int = option[self.human_player]['health'] + option[self.human_player]['shields']
+                        best_option_product: int = best_option[self.human_player]['health'] + best_option[self.human_player]['shields']
 
-        else:
-            # Process human turn
-            spell: str = self.player.input()
-            option: dict = [node for node in self.turns[self.turn] if node['spell'] == spell][0]
-            self.human_health = option[self.human_player]['health']
-            self.human_shields = option[self.human_player]['shields']
-            self.ai_health = option[self.ai_player]['health']
-            self.ai_shields = option[self.ai_player]['shields']
-            self.previous_id = option['id']
+                        if option_product < best_option_product:
+                            best_option = option
 
-        print(f"Player {options[0]['player']} attacks with {spell}")
-        print(f'Human health = {self.human_health}, shields = {self.human_shields}\nAI health = {self.ai_health}, shields = {self.ai_shields}\nTurn {self.turn} concluded')
-        print('____________________________________')
+                spell: str = best_option['spell']
+                self.human_health = best_option[self.human_player]['health']
+                self.human_shields = best_option[self.human_player]['shields']
+                self.ai_health = best_option[self.ai_player]['health']
+                self.ai_shields = best_option[self.ai_player]['shields']
+                self.id = best_option['id']
 
-        self.turn = self.turn + 1
+            else:
+                # Process human turn
+                # option: dict = [node for node in options if node['spell'] == spell][0]
+                option: list[dict] = options
+                # Initial hope here was to simply get the correct option by spell name
+                # But during dedup more than one noce can appear with the same
+                # Must brute the option by calculating the turn product (logic oversight in initial development)
+                op_1: dict = option[0]
+
+                hp_diff: int = abs(op_1[self.ai_player]['health'] - self.ai_health)
+                sh_diff: int = abs(op_1[self.ai_player]['shields'] - self.ai_shields)
+                product: int = hp_diff + sh_diff
+                print(f'product is {product}')
+                # Each attack can produce a unique product, check which one is viable
+                if spell == 'fire' and product == 10:
+                    option = option[0]
+
+                elif spell == 'fire':
+                    option = option[1]
+
+                if spell == 'frost' and (product == 15 or product == 5):
+                    option = option[0]
+
+                elif spell == 'frost':
+                    option = option[1]
+
+                self.human_health = option[self.human_player]['health']
+                self.human_shields = option[self.human_player]['shields']
+                self.ai_health = option[self.ai_player]['health']
+                self.ai_shields = option[self.ai_player]['shields']
+                self.id = option['id']
+
+            print(f"Player {options[0]['player']} attacks with {spell}")
+            print(f'Human health = {self.human_health}, shields = {self.human_shields}\nAI health = {self.ai_health}, shields = {self.ai_shields}\nTurn {self.turn} concluded')
+            print('____________________________________')
+
+            self.turn = self.turn + 1
---
+++ GameGUI.py
@@ -0,0 +1,181 @@
+import tkinter as tk
+from tkinter import ttk
+from Player import Player
+from Game import Game
+from TurnTree import TurnTree
+
+
+class GameGUI:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Game")
+        self.root.geometry("400x300")
+        self.chosen_player = None
+        self.chosen_first = None
+        self.default_progress = 100
+
+        self.game: Game
+        self.turns: dict
+
+        self.choose_player_label = tk.Label(root, text="Choose Player:")
+        self.choose_player_label.pack()
+
+        self.button_frame1 = tk.Frame(root)
+        self.player1_button = tk.Button(self.button_frame1, text="Player 1", command=lambda: self.select_player('p1'))
+        self.player1_button.pack(side="left", padx=5)
+        self.player2_button = tk.Button(self.button_frame1, text="Player 2", command=lambda: self.select_player('p2'))
+        self.player2_button.pack(side="left", padx=5)
+        self.button_frame1.pack(pady=10)
+
+        self.choose_first_label = tk.Label(root, text="Choose who goes first:", anchor="w")
+
+        self.button_frame2 = tk.Frame(root)
+        self.player1_first_button = tk.Button(self.button_frame2, text="Player 1", command=lambda: self.select_first('p1'))
+        self.player1_first_button.pack(side="left", padx=5)
+        self.player2_first_button = tk.Button(self.button_frame2, text="Player 2", command=lambda: self.select_first('p2'))
+        self.player2_first_button.pack(side="left", padx=5)
+
+        self.spell_label = tk.Label(root, text="Choose element:", anchor="w")
+
+        self.spell_frame = tk.Frame(root)
+        self.fire_button = tk.Button(self.spell_frame, text="Fire", command=lambda: self.select_spell("fire"))
+        self.fire_button.pack(side="left", padx=5)
+        self.frost_button = tk.Button(self.spell_frame, text="Frost", command=lambda: self.select_spell("frost"))
+        self.frost_button.pack(side="left", padx=5)
+
+        self.progress_label_player_health = tk.Label(root, text="Placeholder", anchor="w")
+        self.progress_bar_player_health = ttk.Progressbar(root, orient="horizontal", length=300, mode="determinate")
+        #self.progress_bar_player_health['value'] = self.default_progress
+
+        self.progress_label_player_shields = tk.Label(root, text="Placeholder", anchor="w")
+        self.progress_bar_player_shields = ttk.Progressbar(root, orient="horizontal", length=300, mode="determinate")
+        #self.progress_bar_player_shields['value'] = self.default_progress
+
+        self.progress_label_ai_health = tk.Label(root, text="Placeholder", anchor="w")
+        self.progress_bar_ai_health = ttk.Progressbar(root, orient="horizontal", length=300, mode="determinate")
+        #self.progress_bar_ai_health['value'] = self.default_progress
+
+        self.progress_label_ai_shields = tk.Label(root, text="Placeholder", anchor="w")
+        self.progress_bar_ai_shields = ttk.Progressbar(root, orient="horizontal", length=300, mode="determinate")
+        #self.progress_bar_ai_shields['value'] = self.default_progress
+
+        self.hide_progress_bar()
+
+        self.is_ini: bool = False
+
+    def select_player(self, player) -> None:
+        self.chosen_player = player
+        self.button_frame1.pack_forget()
+        self.choose_first_label.pack()
+        self.button_frame2.pack(pady=10)
+
+        self.choose_player_label.config(text=f'Choose Player: {self.chosen_player}')
+        self.choose_first_label.pack()
+
+    def select_first(self, first) -> None:
+        self.chosen_first = first
+        self.button_frame2.pack_forget()
+        self.show_progress_bar()
+
+        self.choose_first_label.config(text=f'Choose who goes first: {self.chosen_first}')
+
+        # All necessary params are now set
+        # Initialize game, generate turns and evaluate them
+        if self.is_ini == False:
+            self.iniGame()
+            self.is_ini = True
+
+            self.progress_label_player_health.config(text=f'Remaining {self.game.human_player} health: {self.game.human_health}')
+            self.progress_bar_player_health['value'] = self.game.human_health
+            self.progress_label_player_shields.config(text=f'Remaining {self.game.human_player} shields: {self.game.human_shields}')
+            self.progress_bar_player_shields['value'] = self.game.human_shields
+            self.progress_label_ai_health.config(text=f'Remaining {self.game.ai_player} health: {self.game.ai_health}')
+            self.progress_bar_ai_health['value'] = self.game.ai_health
+            self.progress_label_ai_shields.config(text=f'Remaining {self.game.ai_player} shields: {self.game.ai_shields}')
+            self.progress_bar_ai_shields['value'] = self.game.ai_shields
+
+
+        self.spell_label.pack()
+        self.spell_frame.pack()
+
+    def iniGame(self) -> None:
+        player_one = Player(ai=False if self.chosen_player == 'p1' else True)
+        player_two = Player(ai=False if self.chosen_player == 'p2' else True)
+
+        # Initialize and generate every possible turn with the above settings
+        player_one_goes_first: bool = True if self.chosen_first == 'p1' else False
+        turn_tree = TurnTree(
+            player_one=player_one,
+            player_two=player_two,
+            player_one_goes_first=player_one_goes_first)
+
+        # Generate every turn outcome
+        turn_tree.generateTree()
+        turn_tree.evaluateTree()
+        self.turns = turn_tree.getTree()
+
+        print(
+            f'Initializing game\nPlayer one health = {player_one.getHealth()}, shields = {player_one.getShields()}\nPlayer two health = {player_two.getHealth()}, shields = {player_two.getShields()}')
+        print(f'You are playing as {self.chosen_player}')
+
+        # Initialize game with params
+        self.game = Game(
+            player=player_one, # doesnt matter which one, we just want the player class methods
+            human_player=self.chosen_player,
+            ai_player=turn_tree.ai_player,
+            turns=self.turns,
+            player_one_goes_first=player_one_goes_first)
+
+    def select_spell(self, spell: str) -> None:
+        if self.game.human_health > 0 and self.game.ai_health > 0:
+            if self.game.turn == 0:
+                self.game.processFirstTurn(spell) # Human turn
+                self.game.processTurn(spell) # AI turn
+                
+            else:
+                self.game.processTurn(spell) # Human turn
+                self.game.processTurn(spell) # AI turn
+
+            self.progress_label_player_health.config(text=f'Remaining {self.game.human_player} health: {self.game.human_health}')
+            self.progress_bar_player_health['value'] = self.game.human_health
+            self.progress_label_player_shields.config(text=f'Remaining {self.game.human_player} shields: {self.game.human_shields}')
+            self.progress_bar_player_shields['value'] = self.game.human_shields
+            self.progress_label_ai_health.config(text=f'Remaining {self.game.ai_player} health: {self.game.ai_health}')
+            self.progress_bar_ai_health['value'] = self.game.ai_health
+            self.progress_label_ai_shields.config(text=f'Remaining {self.game.ai_player} health: {self.game.ai_shields}')
+            self.progress_bar_ai_shields['value'] = self.game.ai_shields
+
+        else:
+            winner: str = self.game.human_player if self.game.human_health > 0 else self.game.ai_player
+            print(f'{winner} has won the game!')
+        self.root.update()
+
+    def show_progress_bar(self) -> None:
+        self.progress_label_player_health.pack()
+        self.progress_bar_player_health.pack()
+        self.progress_label_player_shields.pack()
+        self.progress_bar_player_shields.pack()
+
+        self.progress_label_ai_health.pack()
+        self.progress_bar_ai_health.pack()
+        self.progress_label_ai_shields.pack()
+        self.progress_bar_ai_shields.pack()
+
+
+    def hide_progress_bar(self) -> None:
+        self.progress_label_player_health.pack_forget()
+        self.progress_bar_player_health.pack_forget()
+        self.progress_label_player_shields.pack_forget()
+        self.progress_bar_player_shields.pack_forget()
+
+        self.progress_label_ai_health.pack_forget()
+        self.progress_bar_ai_health.pack_forget()
+        self.progress_label_ai_shields.pack_forget()
+        self.progress_bar_ai_shields.pack_forget()
+
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    game = GameGUI(root)
+    root.mainloop()
